9ca3e05abfd452efaa6ecab982a68714b87a3acd
Jacek Bylina <jacek.bylina@gmail.com>
2016-12-26 20:44:04 +0100

    Add simple post sentiment analysis

--- fb_processor/sentiment_analyser.py
+++ fb_processor/sentiment_analyser.py
@@ -1,9 +1,13 @@
+import re
+import itertools as it
 from urllib.parse import urlparse, parse_qs
 from configparser import ConfigParser
 from facebook import GraphAPI
 
 
 class SentimentAnalyser:
+    FB_URL_PATTERN = re.compile(
+        '(?:(?:http|https):\/\/)?(?:www.)?(mbasic.facebook|m\.facebook|facebook|fb)\.(com|me)\/')
 
     def __init__(self, cfg_dict=None):
         if cfg_dict is None:
@@ -12,20 +16,50 @@ class SentimentAnalyser:
             cfg_dict = cfg_dict['default']
 
         self.facebook = GraphAPI()
-        self.facebook.access_token = self.facebook.get_app_access_token(cfg_dict['FB_APP_ID'], cfg_dict['FB_APP_SECRET'])
+        self.facebook.access_token = self.facebook.get_app_access_token(cfg_dict['FB_APP_ID'],
+                                                                        cfg_dict['FB_APP_SECRET'])
 
-    def process_comment(self, text):
+    def process_fanpage(self, fanpage: str, post_limit: int = 100):
+        fanpage = self.get_fanpage_name(fanpage)
+
+        i = 0
+        for post in self.get_all_posts(fanpage):
+            tmp = self.process_post(post)
+            if tmp is not None:
+                return tmp
+            i += 1
+            if i > post_limit:
+                break
+
+    def process_post(self, post: dict):
+        comments_sentim = list(map(lambda com: self.get_text_sentiment(com['message']),
+                                   self.get_all_comments(post['id'])))
+        if len(comments_sentim) == 0:
+            return None
+        else:
+            return sum(comments_sentim)/len(comments_sentim)
+
+    def get_fanpage_name(self, fanpage: str):
+        if self.FB_URL_PATTERN.match(fanpage, 0):
+            sub_url = self.FB_URL_PATTERN.sub('', fanpage)
+            if sub_url.endswith('/'):
+                return sub_url.replace('/', '')
+            else:
+                return sub_url.rsplit('/', 1)[0].replace('/', '')
+        return fanpage.replace('/', '')
+
+    def get_text_sentiment(self, text: str):
         from textblob import TextBlob
         blob = TextBlob(text)
         return blob.sentiment.polarity
 
-    def get_all_posts(self, page_name):
+    def get_all_posts(self, page_name: str):
         return self.__get_all_connections(page_name, 'posts')
 
-    def get_all_comments(self, post_id):
+    def get_all_comments(self, post_id: str):
         return self.__get_all_connections(post_id, 'comments')
 
-    def __get_all_connections(self, id, connection, **args):
+    def __get_all_connections(self, id: str, connection: str, **args):
         assert id is not None
         assert connection is not None
 
--- tests/test_sentiment_analyser.py
+++ tests/test_sentiment_analyser.py
@@ -1,3 +1,5 @@
+import pytest
+
 from fb_processor.sentiment_analyser import *
 import itertools
 
@@ -6,7 +8,7 @@ class TestSentimentAnalyser:
     tested_obj = SentimentAnalyser()
 
     def test_should_get_ten_posts(self):
-        obj = self.tested_obj.get_all_posts('WirtualnaPolska')
+        obj = self.tested_obj.get_all_posts('CNNInternationalPoland')
         obj = list(itertools.islice(obj, 10))
 
         assert len(obj) == 10
@@ -26,13 +28,31 @@ class TestSentimentAnalyser:
     def test_should_get_negative_text_polarity(self):
         text = 'Bad idea! I hate you'
 
-        val = self.tested_obj.process_comment(text)
+        val = self.tested_obj.get_text_sentiment(text)
         print(val)
         assert val < 0
 
     def test_should_get_positive_text_polarity(self):
         text = 'I really like this :)'
 
-        val = self.tested_obj.process_comment(text)
+        val = self.tested_obj.get_text_sentiment(text)
         print(val)
         assert val > 0
+
+    @pytest.mark.parametrize("text_input,expected_fanpage_name", [
+        ('https://www.facebook.com/CNNInternationalPoland/?fref=ts', 'CNNInternationalPoland'),
+        ('https://www.facebook.com/CNNInternationalPoland/', 'CNNInternationalPoland'),
+        ('https://www.facebook.com/CNNInternationalPoland', 'CNNInternationalPoland'),
+        ('http://www.facebook.com/CNNInternationalPoland', 'CNNInternationalPoland'),
+        ('CNNInternationalPoland', 'CNNInternationalPoland'),
+        ('facebook.com/CNNInternationalPoland', 'CNNInternationalPoland'),
+        ('facebook.com/CNNInternationalPoland/', 'CNNInternationalPoland')
+    ])
+    def test_should_return_fanpage_name_from_url(self, text_input, expected_fanpage_name):
+        fanpage_name = self.tested_obj.get_fanpage_name(text_input)
+        assert fanpage_name == expected_fanpage_name
+
+    def test_should_process_fanpage(self):
+        mean = self.tested_obj.process_fanpage('facebook.com/CNN/', post_limit=10)
+        print(mean)
+        assert mean > 0
